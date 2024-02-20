52e5e1aaa1d0d19e89af589ce0c2f5cf90e89be5
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-02-20 13:09:30 +0330

    add joi to project and remove express-validator

--- src/routes/word.js
+++ src/routes/word.js
@@ -1,29 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { body, query } = require("express-validator");
 const { fetchRandomWord, test } = require("../controllers/word");
 
-router.route("/words/word").get(
-    query("category")
-        .notEmpty()
-        .withMessage("category can not be empty")
-        .trim()
-        .toUpperCase()
-        .isIn(["TA", "TO", "TJ", "TC", "TAC", "TE"])
-        .withMessage(
-            "please select one of this format ['TA', 'TO', 'TJ', 'TC', 'TAC', 'TE'] for level "
-        ),
-    query("level")
-        .notEmpty()
-        .withMessage("level can not be empty")
-        .trim()
-        .isIn(["1", "2", "3"])
-        .withMessage(
-            "please select one of this format ['1','2','3'] for level "
-        ),
-
-    fetchRandomWord
-);
+router.route("/words/word").get(fetchRandomWord);
 router.route("/test").get(test);
 
 module.exports = router;
