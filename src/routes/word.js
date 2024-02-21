e714e3e5778e3fb56dc7f4f3a0c2692c71e48c9c
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-02-21 17:29:36 +0330

    new route for new player

--- src/routes/word.js
+++ src/routes/word.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { fetchRandomWord, test } = require("../controllers/word");
+const { fetchRandomWord, createPlayer, test } = require("../controllers/word");
 
 router.route("/words/word").get(fetchRandomWord);
+router.route("/players/new-player").post(createPlayer);
 router.route("/test").get(test);
 
 module.exports = router;
