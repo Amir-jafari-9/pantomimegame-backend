ffe045abd6cf440f119f48008d8439b6ffe119fd
FacelessFK <fardin.k.s.10.1@gmail.com>
2024-03-13 13:21:06 +0330

    fix level validation

--- src/validators/dto/levelJoi.dto.js
+++ src/validators/dto/levelJoi.dto.js
@@ -7,10 +7,10 @@ module.exports = {
         .required()
         .error(new Error("please provide a category"))
         .trim()
-        .valid("1", "2", "3")
+        .valid("1", "2", "3", "4")
         .error(
             new Error(
-                "please select one of this format ['1','2','3'] for level "
+                "please select one of this format ['1','2','3','4'] for level "
             )
         )
 };
